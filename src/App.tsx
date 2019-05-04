1604824067a292b050a0e1c4300d22108abebe65
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-05-05 08:32:34 +0900

    props & state

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,43 @@ import './App.css';
 import GoogleMapReact from 'google-map-react'
 const key: string = 'your key';
 
-interface Iprops {
-  center: any;
-  zoom: number;
+interface ITextProps {
+  cnt: number
 }
-class App extends React.Component<Iprops> {
+
+class Text extends React.Component<ITextProps, {}> {
   render() {
     return (
-      <div style={{height: '100vh', width: '100%'}}>
-        <GoogleMapReact
-          bootstrapURLKeys={{key: key}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-        </GoogleMapReact>
+      <span>{this.props.cnt}</span>
+    )
+  }
+}
+
+
+interface IAppState {
+  cnts: number[]
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props:any) {
+    super(props)
+    this.state = {
+      cnts: [0,0]
+    };
+    this.click = this.click.bind(this);
+  }
+  click() {
+    this.setState({
+      cnts: this.state.cnts.map(cnt => cnt+1)
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.cnts.map((cnt, index) =>
+          <Text cnt={cnt} key={index} />
+        )}
+        <button onClick={this.click}>click</button>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let center = {
-  lat: 59.95,
-  lng: 30.33
-};
-let zoom = 11;
-ReactDOM.render(<App center={center} zoom={zoom}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
